5f170b2b2cc53c27e80537b78fc7ff16f1f8e5ba
Yamone <yamonethanthar0987@gmail.com>
2025-11-30 11:56:50 +0630

    add new3.py

---
+++ new3.py
@@ -0,0 +1,21 @@
+#name = "Aung Aung"
+#age = 26 
+#print( f"{name} is {age} year old.")
+
+#name = input("waht is your name?\n")
+#age = input("How old are you? \n")
+#print( f"{name} is {age} year old.")
+
+num1 = input("Enter Number1 \n ")
+num2 = input("Enter Number2 \n")
+
+Add = int(num1)+int(num2)
+Sub = int(num1)-int(num2)
+mul = int(num1)*int(num2)
+div = int(num1)/int(num2)
+
+
+print(f"Your Addition answers is {Add}.")
+print(f"Your Subtract answers is {Sub}.")
+print(f"Your multiplication answers is {mul}.")
+print(f"Your division answers is {div}.")
