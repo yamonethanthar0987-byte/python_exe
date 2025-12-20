c3988d2e2e95045c90d2f5c061ae06c201272f87
Yamone <yamonethanthar0987@gmail.com>
2025-12-20 11:48:40 +0630

    Graphic

---
+++ graphic.py
@@ -0,0 +1,22 @@
+import turtle
+
+screen = turtle.Screen()
+screen.bgcolor("blue")
+screen.title("My Turtle Program")
+
+t = turtle.Turtle()
+t.color("white")
+t.shape("turtle")
+t.speed(1)
+
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+
+
+
+screen.mainloop()
